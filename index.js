83f129125acdc8247e18c8deac2818810d618ca9
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-12-14 11:13:43 -0600

    commit tree 7c7f30cc44da798303bca602922721ba3cb804f2

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (blocks) {
+    let distance = Math.abs(blocks - 42);
+    return distance;
+}
+
+function distanceFromHqInFeet(blocks) {
+    let distance = Math.abs((blocks - 42) * 264);
+    return distance;
+}
+
+function distanceTravelledInFeet(start, end) {
+    let distance = Math.abs((start - end) * 264);
+    return distance;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = Math.abs((start - destination) * 264);
+    if (distance <= 400) {
+        return 0;
+    }
+    else if (distance >= 400 && distance < 2000) {
+        return (distance - 400) * .02;
+    }
+    else if (distance > 2000 && distance <= 2500) {
+        return distance = 25;
+    }
+    else if (distance > 2500) {
+        return distance = "cannot travel that far"
+    }
+}
